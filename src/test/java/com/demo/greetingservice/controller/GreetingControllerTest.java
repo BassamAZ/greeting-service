98d9a2592a0eb42d0aa6cc63cf06fb2e1e433b9d
Bassam AbouZaid <>
2020-02-02 21:06:18 +0100

    Updating test cases

--- src/test/java/com/demo/greetingservice/controller/GreetingControllerTest.java
+++ src/test/java/com/demo/greetingservice/controller/GreetingControllerTest.java
@@ -27,25 +27,25 @@ public class GreetingControllerTest {
     @Test
     public void testGreetingWithPersonalAccount() {
         Mockito.when(greetingService.getGreeting(AccountType.PERSONAL.getValue(), 123, null)).thenReturn("Hi, userId 123");
-        Assert.assertEquals(greetingController.getGreeting(AccountType.PERSONAL.getValue(), 123, null), "Hi, userId 123");
+        Assert.assertEquals("Hi, userId 123", greetingController.getGreeting(AccountType.PERSONAL.getValue(), 123, null));
     }
 
     @Test
     public void testGreetingWithNegativeID() {
         Mockito.when(greetingService.getGreeting(AccountType.PERSONAL.getValue(), -1, null)).thenReturn("The id should be positive value");
-        Assert.assertEquals(greetingController.getGreeting(AccountType.PERSONAL.getValue(), -1, null), "The id should be positive value");
+        Assert.assertEquals("The id should be positive value", greetingController.getGreeting(AccountType.PERSONAL.getValue(), -1, null));
     }
 
     @Test
     public void testGreetingWithValidBusinessAccount() {
         Mockito.when(greetingService.getGreeting(AccountType.BUSINESS.getValue(), 333, "big")).thenReturn("Welcome, business user!");
-        Assert.assertEquals(greetingController.getGreeting(AccountType.BUSINESS.getValue(), 333, "big"), "Welcome, business user!");
+        Assert.assertEquals("Welcome, business user!", greetingController.getGreeting(AccountType.BUSINESS.getValue(), 333, "big"));
     }
 
     @Test
     public void testConstraintViolationHandlerWithInvalidData() {
         ConstraintViolationException constraintViolationException = new ConstraintViolationException("This is Constraint Violation msg", null);
-        Assert.assertEquals(greetingController.constraintViolationHandler(constraintViolationException), "no constraints violations message");
+        Assert.assertEquals("no constraints violations message", greetingController.constraintViolationHandler(constraintViolationException));
     }
 
 }
