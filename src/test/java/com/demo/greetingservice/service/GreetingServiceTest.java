0e3b4151b8ff41aa7fe5b006b79e4e922db5d5cf
Bassam AbouZaid <>
2020-02-02 21:30:37 +0100

    Updating test for GreetingService

---
+++ src/test/java/com/demo/greetingservice/service/GreetingServiceTest.java
@@ -0,0 +1,28 @@
+package com.demo.greetingservice.service;
+
+import com.demo.greetingservice.model.AccountType;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GreetingServiceTest {
+
+    @Autowired
+    private GreetingService greetingService;
+
+    @Test
+    public void testGreetingServiceWithPersonalAccount() {
+        Assert.assertEquals("Hi, userId 123", greetingService.getGreeting(AccountType.PERSONAL.getValue(), 123, null));
+    }
+
+    @Test
+    public void testGreetingServiceWithValidBusinessAccount() {
+        Assert.assertEquals("Welcome, business user!", greetingService.getGreeting(AccountType.BUSINESS.getValue(), null, "big"));
+    }
+
+}
